47962f776dd23bea1a7111267fccfde19697a0c6
villafrancanjelo <jelo.villafranca@gmail.com>
2021-11-14 16:36:00 +0800

    for routing to login and register

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: RegisterComponent},
+ 
+
+{path: "**", redirectTo: 'login'}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +22,10 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      routes
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
