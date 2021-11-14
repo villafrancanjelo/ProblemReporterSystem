2e3216a9984e1c102383d1cf1ac2f4ab5797b9a0
villafrancanjelo <jelo.villafranca@gmail.com>
2021-11-14 17:15:05 +0800

    modules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'profile', component: RegisterComponent},
+  {path: 'register', component: RegisterComponent},
  
 
 {path: "**", redirectTo: 'login'}
@@ -23,6 +30,10 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     RouterModule.forRoot(
       routes
     ),
